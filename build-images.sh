21ec9ac13604af7c1b7d62c7f2457b870f4c1f4d
mrmarkuz <mrmarkuz666@gmail.com>
2025-02-13 12:19:48 +0100

    Fix onlyoffice version to 8.3.0.1

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/onlyoffice/documentserver:8.3.1.10" \
+    --label="org.nethserver.images=docker.io/onlyoffice/documentserver:8.3.0.1" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
